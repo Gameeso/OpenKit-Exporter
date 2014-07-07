76971b3de17921fc7aae38061ed556ea20f7cb18
Peter Wilemsen <peter@codebuffet.co>
2014-07-08 00:37:04 +0200

    We"re getting somewhere!

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
+# -*- coding: utf-8 -*-
+
 ## OpenKit Exporter
 ## Created by Peter Willemsen <peter@codebuffet.co>
 
-def main():
-    server = input_default("Server", "http://openkit.io")
-    app_key = input_default("Application Key", None)
-    app_secret = input_default("Application Secret", None)
-
-    import_from_server(server, app_key, app_secret)
-
-def import_from_server(server, app_key, app_secret):
-    print server, app_key, app_secret
-
+from requests_oauthlib import OAuth1Session
+import json
 
 class bcolors:
     HEADER = '\033[95m'
@@ -19,6 +13,74 @@ class bcolors:
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
+    GREY = '\033[37m'
+
+def intro():
+    print """
+                      Proudly build by%s
+                        ███████████
+                     █████████████████
+                     █████████████████                      
+                     ███           ███                      
+                     ███           ███                      
+                     █████████████████                      
+                ███████████████████████████                
+           █████████████████████████████████████            
+           █████████████████████████████████████            
+        ██████████████░░░░░█████░░░░░██████████████         
+     █████████████████░░░░░█████░░░░░████████░░░██████      
+     ██████████████░░░░░███████████░░░░░████████░░░███      
+     ██████████████░░░░░███████████░░░░░████████░░░███      
+  █████████████████░░░░░███████████░░░░░████████░░░░░░███   
+  █████████████████░░░░░███████████░░░░░████████░░░░░░███   
+  ████████████████████░░░░░█████░░░░░███████████░░░░░░███   
+██████████████████████░░░░░█████░░░░░██████████████░░░░░░███
+███████████████████████████████████████████████████░░░░░░███
+███████████████████████████████████████████████████░░░░░░███
+                                                            
+                                                            
+███████████████████████████████████████████████████░░░░░░███
+███████████████████████████████████████████████████░░░░░░███
+███████████████████████████████████████████████████░░░░░░███%s
+""" % (bcolors.GREY, bcolors.ENDC)
+
+    print """
+Welcome to the Export tool for OpenKit!
+This tool allows you to locally store all the data we can get from OpenKit, so you can keep it safe after the 1 december deadline.
+
+After Gameeso is done, I (or we depending on how the community grows :)) will allow you to import that data again in Gameeso to continue your services.
+Please follow the instructions along.
+
+This tool functions like a insurance policy, to keep at least your data safe whenether or not I decide to continue with Gameeso.
+
+With great love, Peter Willemsen.
+More Info: http://gameeso.com/openkit-notes/
+        """
+
+def main():
+    intro()
+    server = input_default("Server", "http://api.openkit.io")
+    app_key = input_default("Application Key", None)
+    app_secret = input_default("Application Secret", None)
+
+    import_from_server(server, app_key, app_secret)
+
+def import_from_server(server, app_key, app_secret):
+    print server, app_key, app_secret
+    session = OAuth1Session(app_key,
+                            client_secret=app_secret)
+
+    output = {}
+
+    def get(endpoint):
+        return session.get("%s/%s" % (server, endpoint))
+
+    leaderboards = json.loads(get("leaderboards?tag=v1").content)
+
+    output['leaderboards'] = leaderboards
+
+    print output
+
 
 def str_type_default(str):
     return '%s%s%s' % (bcolors.OKBLUE, str, bcolors.ENDC)
